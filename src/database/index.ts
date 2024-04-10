cb47e90e8717985d8435c84267e669fab8343d6b
Keanu <keanuharrell@hotmail.fr>
2024-04-10 11:01:23 +0200

    update database schema

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,13 +2,14 @@ import { IRestaurant } from 'cesieats-service-types/src/restaurant';
 import mongoose, { Schema, model } from 'mongoose';
 
 const restaurantSchema = new Schema<IRestaurant>({
-  title: { type: String, required: true },
+  name: { type: String, required: true },
   description: { type: String, required: true },
   address: { type: String, required: true },
-  openingTime: { type: Number, required: true },
-  closingTime: { type: Number, required: true },
+  openingTime: { type: String, required: true },
+  closingTime: { type: String, required: true },
   deliveryPrice: { type: Number, required: true },
-  telephone: { type: Number, required: true },
+  telephone: { type: String, required: true },
+  idAccount: { type: String, required: true },
 });
 
 export const Restaurant = model<IRestaurant>('Restaurant', restaurantSchema);
